49663d7c7c611edce2e1c4aa1b21cb59a9bab03f
Alif Pahreza <agzmv123@gmail.com>
2024-06-09 06:34:28 +0800

    update: fix navbar link

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -1,47 +1,62 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import logoBrand from '../../assets/icons/logoBrand.png';
 import { FiHome } from 'react-icons/fi';
 import { TfiPencilAlt } from 'react-icons/tfi';
 import { MdOutlineKeyboardArrowDown, MdOutlinePostAdd } from 'react-icons/md';
 import avatarProfile from '../../assets/images/profile-pic (4).png';
+import PropTypes from 'prop-types';
 
-export default function Index() {
+export default function Index({ openModal, isOpenModal }) {
+	const location = useLocation();
+
 	return (
 		<>
-			<header className='lg:px-10 px-5 hidden md:block border-b border-[#424242] bg-navbar'>
+			<header className='lg:px-10 px-5 hidden sm:block border-b border-[#424242] bg-navbar'>
 				<div className='flex justify-between lg:container'>
-					<img src={logoBrand} alt='logo brand' className='relative -top-3' />
+					<Link to='/'>
+						<img src={logoBrand} alt='logo brand' className='relative -top-3' />
+					</Link>
 					<nav className='flex items-center gap-10 px-6 text-textSecondary'>
-						<NavLink
-							activeClassName='active'
-							className='flex items-start gap-2 text-base font-medium'
+						<Link
+							className={`flex items-start gap-2 text-base font-semibold transition-all duration-200 ease-out hover:opacity-80 ${
+								isOpenModal || location.pathname !== '/' ? '' : 'active'
+							}`}
 							to='/'
 						>
 							<FiHome title='Home' className='text-3xl lg:text-xl' />
 							<p className='hidden lg:block'>Home</p>
-						</NavLink>
-						<NavLink
-							activeClassName='active'
-							className='flex gap-3 text-base font-medium '
-							to='/write-progress'
+						</Link>
+						<Link
+							className={`flex gap-3 text-base font-semibold transition-all duration-200 ease-out hover:opacity-80 ${
+								isOpenModal ? 'active' : ''
+							}`}
+							onClick={() => {
+								sessionStorage.setItem('openModal', 'true')
+								openModal(true);
+							}}
+							to='/'
 						>
 							<TfiPencilAlt title='Write Progress' className='text-2xl lg:text-xl' />
 							<p className='hidden lg:block'>Write Progress</p>
-						</NavLink>
-						<NavLink
+						</Link>
+						<Link
 							to='/post-portfolio'
-							activeClassName='active'
-							className='flex gap-2 text-base font-medium'
+							className={`flex gap-2 text-base font-semibold transition-all duration-200 ease-out hover:opacity-80 ${
+								location.pathname === '/post-portfolio' ? 'active' : ''
+							}`}
 						>
-							<MdOutlinePostAdd title='Post Portfolio' className='text-4xl lg:text-2xl' />
+							<MdOutlinePostAdd
+								title='Post Portfolio'
+								className='text-4xl lg:text-2xl'
+							/>
 							<p className='hidden lg:block'>Post Portfolio</p>
-						</NavLink>
-						<div className='flex items-center gap-2'>
+						</Link>
+						<div className='flex items-center gap-2 cursor-pointer'>
 							<img
 								src={avatarProfile}
 								alt='avatar'
-								className='object-cover w-10 h-10 rounded-full lg:w-7 lg:h-7'
+								className='object-cover w-10 h-10 transition-all duration-200 ease-out rounded-full lg:w-7 lg:h-7 hover:opacity-80'
 							/>
 							<p className='text-xl lg:text-base'>Me</p>
 							<MdOutlineKeyboardArrowDown />
@@ -49,18 +64,36 @@ export default function Index() {
 					</nav>
 				</div>
 			</header>
-			<footer className='fixed bottom-0 w-full py-1 border-t border-[#424242] bg-navbar flex md:hidden'>
+			<footer className='fixed bottom-0 z-50 w-full py-1 border-t border-[#424242] bg-navbar flex sm:hidden'>
 				<div className='container flex items-center justify-around py-3 text-textSecondary'>
-					<NavLink activeClassName='active' to='/'>
+					<NavLink
+						activeclassname='active'
+						className='transition-all duration-200 ease-out hover:opacity-80'
+						to='/'
+					>
 						<FiHome title='Home' className='text-2xl' />
 					</NavLink>
-					<NavLink activeClassName='active' to='/write-progress'>
-						<TfiPencilAlt title='Write Progress' className='text-2xl'/>
+					<button
+						color=''
+						className={`flex gap-3 text-base font-semibold transition-all duration-200 ease-out hover:opacity-80 ${
+							isOpenModal ? 'active' : ''
+						}`}
+						onClick={() => {
+							sessionStorage.setItem('openModal', 'true')
+							openModal(true);
+						}}
+						to='/'
+					>
+						<TfiPencilAlt title='Write Progress' className='text-2xl lg:text-xl' />
+					</button>
+					<NavLink
+						activeclassname='active'
+						className='transition-all duration-200 ease-out hover:opacity-80'
+						to='/post-portfolio'
+					>
+						<MdOutlinePostAdd title='Post Portfolio' className='text-3xl' />
 					</NavLink>
-					<NavLink activeClassName='active' to='/post-portfolio'>
-						<MdOutlinePostAdd title='Post Portfolio' className='text-3xl'/>
-					</NavLink>
-					<div className='flex flex-col items-center'>
+					<div className='flex flex-col items-center transition-all duration-200 ease-out cursor-pointer hover:opacity-80'>
 						<img
 							src={avatarProfile}
 							alt='avatar'
@@ -72,3 +105,8 @@ export default function Index() {
 		</>
 	);
 }
+
+Index.propTypes = {
+	openModal: PropTypes.func,
+	isOpenModal: PropTypes.bool,
+};
