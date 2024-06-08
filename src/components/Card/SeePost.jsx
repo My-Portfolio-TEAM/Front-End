0358582cef08be8687b1232c86ad0f00423c3ed4
Alif Pahreza <agzmv123@gmail.com>
2024-06-09 06:55:22 +0800

    update: fix bug filter post

--- src/components/Card/SeePost.jsx
+++ src/components/Card/SeePost.jsx
@@ -1,36 +1,49 @@
 import React, { useState } from 'react';
 import { Dropdown } from 'flowbite-react';
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import PropTypes from 'prop-types';
 
-export default function SeePost() {
-	const [selectedPost, setSelectedPost] = useState('All Post');
+export default function SeePost({ postType }) {
+	const [selectedPost, setSelectedPost] = useState('All Posts');
 
+	const selectPost = (post) => {
+		setSelectedPost(post);
+		postType(post);
+	};
 
 	return (
 		<Dropdown
 			dismissOnClick
 			renderTrigger={() => (
-				<span className='flex items-end flex-1 gap-0 px-3 mt-2 text-sm font-semibold cursor-pointer md:px-1 w-fit hover:text-textSecondary text-textPrimary'>
+				<span className='flex gap-0 px-3 mt-2 text-sm font-semibold cursor-pointer md:px-1 w-fit hover:text-textSecondary text-textPrimary'>
 					{selectedPost}
-					<MdOutlineKeyboardArrowDown className='text-xl' />
+					<MdOutlineKeyboardArrowDown className={'text-xl transition-all'} />
 				</span>
 			)}
-			className='flex gap-10 shadow-2xl border-2 border-[#262626] cursor-pointer bg-searchInput text-textPrimary'
+			className='shadow-2xl border-[#262626] cursor-pointer bg-eerieBlack text-textPrimary'
 		>
 			<Dropdown.Item
-				onClick={() => setSelectedPost('All Post')}
-				className={`text-textPrimary ${selectedPost === 'All Post' ? 'bg-eerieBlack' : ''}`}
-			>
-				All Post
-			</Dropdown.Item>
-			<Dropdown.Item
-				onClick={() => setSelectedPost('Your Post')}
+				style={{ backgroundColor: '#1A1C20' }}
+				onClick={() => selectPost('All Posts')}
 				className={`text-textPrimary ${
-					selectedPost === 'Your Post' ? 'bg-eerieBlack' : ''
+					selectedPost === 'All Posts' ? 'border-s border-ufoGreen' : ''
 				}`}
 			>
-				Your Post
+				All Posts
+			</Dropdown.Item>
+			<Dropdown.Item
+				style={{ backgroundColor: '#1A1C20' }}
+				onClick={() => selectPost('My Posts')}
+				className={`text-textPrimary ${
+					selectedPost === 'My Posts' ? 'border-s border-ufoGreen' : ''
+				}`}
+			>
+				My Posts
 			</Dropdown.Item>
 		</Dropdown>
 	);
 }
+
+SeePost.propTypes = {
+	postType: PropTypes.func.isRequired,
+};
